39485610ac2be5503df972afebdd97c96a9f190d
Leslie <leslieasante44@gmail.com>
2023-10-11 14:26:40 -0700

    Am I doing it right?

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -7,20 +7,19 @@
 
 int main(void)
 {
-        int i;
+	int i;
 
-        printf("Infinite loop incoming :(\n");
+	printf("Infinite loop incoming :(\n");
+	/**
+	*i = 0;
+	*
+	*while (i < 10)
+	*{
+	*       putchar(i);
+	*}
+	*/
+	printf("Infinite loop avoided! \\o/\n");
 
-        i = 0;
-
-        while (i < 10)
-        {
-                putchar(i);
-        }
-
-        printf("Infinite loop avoided! \\o/\n");
-
-        return (0);
-	//i does not get incremented/
+	return (0);
 
 }
