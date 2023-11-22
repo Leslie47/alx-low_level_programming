87a469d188c19a9b0cdd25ccfa29bf4a5b4bffb2
Leslie <leslieasante44@gmail.com>
2023-11-22 15:40:08 -0800

    task 102

---
+++ 0x13-more_singly_linked_lists/102-free_listint_safe.c
@@ -0,0 +1,34 @@
+#include "lists.h"
+
+/**
+ * print_listint_safe - Prints a listint_t linked list safely.
+ * @head: Pointer to the head of the list.
+ *
+ * Return: The number of nodes in the list.
+ */
+
+size_t free_listint_safe(listint_t **h)
+{
+	size_t count = 0;
+	listint_t *current, *next;
+
+	if (h == NULL || *h == NULL)
+	{
+		return (0);
+	}
+	current = *h;
+	while (current)
+	{
+		count++;
+		next = current->next;
+		free(current);
+
+		if (current <= next)
+		{
+			break;
+		}
+		current = next;
+	}
+	*h = NULL;
+	return (count);
+}
